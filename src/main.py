85e2580d9e53a4145822cee0e242c05a55197872
PurpleLobster31 <lopesmatheus642@gmail.com>
2022-11-07 09:28:22 -0300

    Adição da função ler_PDF()

--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
 import threading
 import PyPDF2 as pydf
 
-def ler_PDF():
-    pass
+def ler_PDF(nome_arquivo):
+    leitor = pydf.PdfFileReader('data/' + nome_arquivo)
+    for num_pagina in range(0, leitor.numPages):
+        pagina = leitor.getPage(num_pagina)
+        conteudo_pagina = pagina.extract_text()
+        paragrafos = conteudo_pagina.split('\n')
 
 if __name__ == "__main__":
-    pass
+    print('Informe a palavra que deseja procurar: ')
+    palavra = input()
+    
+    
