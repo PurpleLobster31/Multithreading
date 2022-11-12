ffc8a50123c8961a38e1be0278b38cad69c8f85f
Matheus Mendonça <lopesmatheus642@gmail.com>
2022-11-12 17:42:22 -0300

    build: build inicial

--- src/main.py
+++ src/main.py
@@ -1,15 +1,56 @@
 import threading
+from os import system, path
 import PyPDF2 as pydf
+from typing import NamedTuple
 
-def ler_PDF(nome_arquivo):
-    leitor = pydf.PdfFileReader('data/' + nome_arquivo)
-    for num_pagina in range(0, leitor.numPages):
-        pagina = leitor.getPage(num_pagina)
-        conteudo_pagina = pagina.extract_text()
-        paragrafos = conteudo_pagina.split('\n')
+from utils import ler_PDF, alimenta_dados
+
+
+contador_palavra = 0
+lock = threading.Lock()
+ocorrencias = []
+
+
+def busca_palavra(info:NamedTuple) -> None:
+    '''
+      função que busca a palavra na linha e adiciona à lista de ocorrências um dicionário
+      :param info: namedtuple (subclasses de tuplas com nomes de campo)
+    '''
+    global contador_palavra
+    global ocorrencias
+    global lock
+    for linha in info.linhas_para_ler:
+        num = linha.count(info.alvo)
+        if num > 0:
+            lock.acquire()
+            contador_palavra += num
+            ocorrencias.append({'Pagina':info.num_pag, 'Linha': linha})
+            lock.release()
 
 if __name__ == "__main__":
-    print('Informe a palavra que deseja procurar: ')
-    palavra = input()
+    print('Digite o nome do arquivo que deseja ler: ')
+    arquivo = input()
     
+    system('cls||clear')
+    print('Informe a palavra ou frase que deseja procurar: ')
+    alvo = input()
     
+    linhas, num_threads = ler_PDF(arquivo)
+    infos = alimenta_dados(alvo, linhas)
+    
+    thread_list = []
+    for item in infos:
+        t = threading.Thread(target=busca_palavra, args=(item,))
+        t.start()
+        thread_list.append(t)
+    
+    for thread in thread_list:
+        thread.join()
+        
+    print(f"Número de ocorrências do termo '{alvo}' no arquivo: {contador_palavra}")
+    print("-"*30)
+    print("| Pg | Conteudo |")
+    for item in ocorrencias:
+        print(f"| {item['Pagina']} | {item['Linha']} |")
+
+#TODO: joao fazer o README E O .PDF
